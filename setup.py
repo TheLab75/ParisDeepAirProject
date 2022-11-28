831806bbc77a42f717fa95250f9d5c522b313d2b
Louis Lablaude <louis.lablaude@live.fr>
2022-11-28 16:01:15 +0100

    Project requirements & setup package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='ParisDeepAirProject_install_package',
+      version="0.1",
+      description="Install_package",
+      license="MIT",
+      author="Louis Lablaude",
+      author_email="-",
+      url="https://github.com/TheLab75/ParisDeepAirProject",
+      install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
