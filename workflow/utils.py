f102b12c2b3cf1d9fe29343622a85de0cc0c2362
Louis Lablaude <louis.lablaude@live.fr>
2022-11-30 09:50:31 +0100

    .py file structure updated

--- workflow/utils.py
+++ workflow/utils.py
@@ -28,3 +28,67 @@ def simple_time_and_memory_tracker(method):
         return result
 
     return method_with_trackers
+
+def analyse_df(df, corr_limit = 0.75):
+    """Analyse any dataframe and print results
+    * Print df Shape, duplicate rows qnt, memory usage, data types and call DataFrame.describe()
+    * Check Missing values in each columns, returning qnt. and percentage
+    * Check Linear Correlation between columns, return Pearson number
+    Keyword arguments:
+    df -- Any DataFrame
+    corr_limit -- Correlation Limit (Pearson) to define if relationship exists (default 0.75)
+    """
+
+    print('General Info:')
+    print(f'{df.shape[0]} Rows {df.shape[1]} Columns'
+          f'\n{df.duplicated().sum()} Duplicated Rows'
+          f'\nMemory Usage: {df.memory_usage().sum()/(1024*1024):.2f}Mb')
+
+    # Checking Data Types
+    int_list, float_list,object_list,bool_list,other_list =[[] for i in range(5)]
+    for col in df.columns:
+        if df[col].dtype == 'int64':
+            int_list.append(col)
+        elif df[col].dtype == 'float64':
+            float_list.append(col)
+        elif df[col].dtype == 'object':
+            object_list.append(col)
+        elif df[col].dtype == 'boolean':
+            bool_list.append(col)
+        else:
+            other_list.append(col)
+
+    for type_list,data_type in zip([int_list, float_list,object_list,bool_list,other_list],
+                                   ['int64','float64','object','boolean','other']):
+        if len(type_list)>0:
+            print(f'\nColumns {data_type}: {type_list}')
+
+    # General statistics
+    display(df.describe())
+
+    # Checking Missing Values in each columns
+    print('\nCheking Missing Values:')
+    col_with_missing_counter = 0
+    for col in df.columns:
+        qnt_missing = df[col].isna().sum()
+        if qnt_missing > 0:
+            col_with_missing_counter +=1
+            print(f'Column "{col}" has {qnt_missing} missing values ({qnt_missing/df.shape[0]:.2%})')
+    if col_with_missing_counter ==0 :
+        print('Analyzed DataFrame has no missing values')
+
+    # Checking linear correlation between columns
+    print('\nChecking Linear Correlation:')
+    df_corr = df.corr() # Correlation DataFrame
+    ckecked_list =[] # Ensure that we won't print the same information twice
+    cols_with_correlation_counter = 0
+    for col in df_corr.columns:
+        ckecked_list.append(col)
+        for i in range(len(df_corr)):
+            if ((df_corr[col][i] > corr_limit or df_corr[col][i] < -corr_limit) and
+                (df_corr.index[i] not in ckecked_list)):
+                cols_with_correlation_counter += 1
+                print(f'Linear Correlation found between columns '
+                      f'{df_corr.index[i]} and {col} -> Pearson coef. = {df_corr[col][i]:.2f}')
+    if cols_with_correlation_counter == 0:
+        print('No linear correlation was found')
