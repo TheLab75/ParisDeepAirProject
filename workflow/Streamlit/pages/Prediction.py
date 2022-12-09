8338dd9d2aa20cbffe37b188f9ade3d961e1ec88
philippe macy <philippe.macy@ieseg.fr>
2022-12-09 12:23:22 +0100

    color graph and affichage

--- workflow/Streamlit/pages/Prediction.py
+++ workflow/Streamlit/pages/Prediction.py
@@ -17,7 +17,7 @@ with st.form(key='params_for_api'):
 
     station = st.selectbox(
         'Select a station  ?',
-        ('Paris_south', 'Paris_east', 'Paris_north','Paris_west','Paris_center'))
+        ('','Paris_south', 'Paris_east', 'Paris_north','Paris_west','Paris_center'))
 
     st.form_submit_button('Running plan')
 
@@ -48,22 +48,7 @@ prediction = response.json()
 
 
 
-# col1, col2, col3,col4 = st.columns(3)
-# col1.metric(label="ATMO J+1 ", value=" ✅ Great",delta=-1)
-# col2.metric(label="ATMO J+2", value=5000, delta=-1000)
-# col3.metric(label="ATMO J+3", value=5000, delta=0)
-# col4.metric(label="ATMO J+4",value="🚫 Bad ",delta =-1 )
 
-# style_metric_cards()
-
-# col5,col6,col7=  st.columns(3)
-# col5.metric(label="ATMO J+5",value="🚫 Bad ",delta =-1 )
-# col6.metric(label="ATMO J+6",value="🚫 Bad ",delta =-1 )
-# col7.metric(label="ATMO J+7",value="🚫 Bad ",delta =-1 )
-# style_metric_cards()
-
-
-#if prediction == 0:
 
 if station != "All":
 
